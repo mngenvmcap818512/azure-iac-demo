33767918a42a3f056240989df6f63b1c7274945d
floureiro@microsoft.com <floureiro@microsoft.com>
2026-03-19 23:12:11 +0000

    push code5

--- demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/vars.tf
+++ demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/vars.tf
@@ -17,5 +17,19 @@ variable "tags" {
     environment = "CICDdemo"
     owner       = "floureiro@microsoft.com"
     landingzone = "demolandzone"
+    project     = "iacdemo"
   }
 }
+
+variable "vnet_name" {
+	description = "Name of the virtual network"
+	type        = string
+	default     = "ne1-spoke-vnet1"
+}
+
+variable "address_spaces" {
+	description = "List of address spaces for the VNet"
+	type        = list(string)
+	default     = ["10.1.0.0/16"]
+}
+
