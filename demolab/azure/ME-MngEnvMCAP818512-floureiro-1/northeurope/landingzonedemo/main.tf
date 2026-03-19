08d3e7ff01108ec1df53447790e8807b34026e42
floureiro@microsoft.com <floureiro@microsoft.com>
2026-03-19 23:12:11 +0000

    push code5

--- demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/main.tf
+++ demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/main.tf
@@ -3,3 +3,14 @@ resource "azurerm_resource_group" "rg1" {
   location = var.location
   tags     = var.tags
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = var.address_spaces
+
+  tags = {
+   environment = "Lab"
+  }
+}
