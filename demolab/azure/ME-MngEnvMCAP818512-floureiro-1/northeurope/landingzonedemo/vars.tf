bed648c359db53177efb9c9190d604a4c1a1bae4
floureiro@microsoft.com <floureiro@microsoft.com>
2026-03-19 22:36:31 +0000

    push code

---
+++ demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/vars.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-ne1-prd-landingzone-networks"
+}
+
+variable "location" {
+  description = "Azure region where the resource group will be created"
+  type        = string
+  default     = "North Europe"
+}
+
+variable "tags" {
+  description = "Tags to apply to the resource group"
+  type        = map(string)
+  default = {
+    environment = "CICDdemo"
+    owner       = "floureiro@microsoft.com"
+    landingzone = "demolandzone"
+  }
+}
