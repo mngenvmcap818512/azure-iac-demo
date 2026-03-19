fa885789f5149711573e02c6bc06c84e1c7cd487
floureiro@microsoft.com <floureiro@microsoft.com>
2026-03-19 23:40:10 +0000

    push code8

--- demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/provider.tf
+++ demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/provider.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
-  use_msi = true
-  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
+  use_oidc = true
 }
