e1b98e28734e5d77d33de16535a421657f5dcc7d
floureiro22 <floureiro@microsoft.com>
2026-03-20 08:52:52 +0000

    Terraform2 (#3)
    * push code5
    
    * push code5
    
    * push code6
    
    * push code7
    
    * push code8
    
    * push code 9
    
    * push code 10
    
    * push code 11
    
    * push code 12
    
    * push code

--- demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/main.tf
+++ demolab/azure/ME-MngEnvMCAP818512-floureiro-1/northeurope/landingzonedemo/main.tf
@@ -10,7 +10,5 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = azurerm_resource_group.rg1.name
   address_space       = var.address_spaces
 
-  tags = {
-   environment = "Lab"
-  }
+  tags = var.tags
 }
